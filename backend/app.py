9225d2600b9195fcad859fa7cb5700c623102a99
18gen <ichihashigen@gmail.com>
2025-01-18 06:38:59 -0500

    backend update

--- backend/app.py
+++ backend/app.py
@@ -8,34 +8,59 @@
 '''
 
 
-from flask import Flask, render_template
-from flask_socketio import SocketIO
-
+from flask import Flask, render_template, request, jsonify
+from flask_socketio import SocketIO, send, emit
 app = Flask(__name__)
-socketio = SocketIO(app)
+socketio = SocketIO(app, cors_allowed_origins="*")
 
-# Route for serving the main page
+# To store WebRTC signaling data temporarily
+cameraSID = None
+
 @app.route('/')
 def index():
-    return render_template('index.html')  # Frontend HTML page
+    # Endpoint to serve the camera streaming page
+    return render_template('index.html')
 
-# WebSocket event for receiving data from the client
-@socketio.on('frame_data')
-def handle_frame_data(data):
-    print(f"Received frame data: {data}")
-    # Example: Send a response back to the client
-    socketio.emit('response', {'message': 'Frame received and processed!'})
+@app.route('/viewer')
+def viewer():
+    # Endpoint to serve the viewer page
+    return render_template('viewer.html')
 
-# WebSocket event for handling connections
 @socketio.on('connect')
 def handle_connect():
-    print('Client connected!')
-    socketio.emit('response', {'message': 'Welcome to the WebSocket server!'})
+    # Event handler for new WebSocket connections
+    # if the client is a camera, store its SID
+    # if the client is a viewer, send the camera SID
+    if cameraSID is not None:
+        emit("requestForOffer", request.sid, to=cameraSID)
+        print("Requesting offer from camera")
 
-# WebSocket event for handling disconnections
-@socketio.on('disconnect')
-def handle_disconnect():
-    print('Client disconnected!')
+@socketio.on('iamcamera')
+def handle_camera():
+    # Event handler for camera identification
+    # store the camera SID
+    global cameraSID
+    cameraSID = request.sid
+    print("Camera connected")
+
+@socketio.on('offer')
+def handle_offer(data):
+    # Event handler for WebRTC offer messages
+    # data['viewer'] is the SID of the viewer
+
+    # send the offer to the viewer
+    emit("requestForAnswerPlOffer", data['offer'], to=data['viewer'])
+    print("Got offer. Requesting answer from viewer and sending offer")
+
+@socketio.on('answer')
+def handle_answer(data):
+    # Event handler for WebRTC answer messages
+    # data['camera'] is the SID of the camera
+
+    # store the answer data for the camera
+    # send the answer to the camera
+    emit("answerFromViewer", {"answer": data['answer'], "viewer": request.sid}, to=cameraSID)
+    print("Got answer from viewer and sending it to camera")
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000)
+    socketio.run(app, debug=True)
